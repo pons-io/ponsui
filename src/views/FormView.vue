cd88937321c4b0f86b97c5a816296b7ac2dd4f17
dhershman1 <dustinh17@gmail.com>
2024-04-23 19:04:34 -0400

    Input Changes

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,42 +1,28 @@
 <template>
   <h1>Example of a form view</h1>
-  <div class="form">
-    <fieldset>
-      <label class="control__label--required">Hello There</label>
-      <input class="control__input" />
-    </fieldset>
-  </div>
-  <div class="control">
-    <div class="control__label">
-      <label class="control__label--required control__label--marked" for="basicUsername">Username</label>
-    </div>
-    <div class="control__item">
-      <div class="control__input__wrapper">
-        <div class="control__input__content">
-          <input class="control__input" id="basicUsername"/>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="control">
-    <div class="control__label">
-      <label class="control__label--required control__label--marked" for="basicPassword">Password</label>
-    </div>
-    <div class="control__item">
-      <div class="control__input__wrapper">
-        <div class="control__input__content">
-          <input class="control__input" id="basicPassword"/>
-        </div>
-      </div>
-    </div>
-  </div>
+  <section class="login-form">
+    <form class="form">
+      <section class="control__flex">
+        <fieldset class="control">
+          <label class="control__label--required">Email</label>
+          <input class="control__input" />
+        </fieldset>
+        <fieldset class="control">
+          <label class="control__label--required">Password</label>
+          <input class="control__input" type="password" />
+        </fieldset>
+      </section>
+    </form>
+  </section>
 </template>
 
 <style>
-.form {
-  width: 50%;
+.login-form {
   display: flex;
-  justify-items: center;
-  align-items: center
+  justify-content: center;
+  align-items: center;
+}
+.form {
+  width: 25%;
 }
 </style>
