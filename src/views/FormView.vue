34f35866bc572aa2199a0c769470aa11395f045f
dhershman1 <dustinh17@gmail.com>
2024-04-24 00:09:56 -0400

    Added: Checkbox ui

--- src/views/FormView.vue
+++ src/views/FormView.vue
@@ -1,3 +1,9 @@
+<script setup>
+import { ref } from 'vue'
+
+const tmp = ref(false)
+</script>
+
 <template>
   <h1>Example of a form view</h1>
   <section class="login-form">
@@ -11,6 +17,15 @@
           <label class="control__label--required">Password</label>
           <input class="control__input" type="password" />
         </fieldset>
+      </section>
+      <section class="control__flex">
+        <label class="checkbox__wrapper">
+          <span :class="['checkbox', { 'checkbox--checked': tmp }]">
+            <input v-model="tmp" type="checkbox" class="checkbox__input">
+            <span class="checkbox__inner"></span>
+          </span>
+          <span>Remember Me</span>
+        </label>
       </section>
     </form>
   </section>
