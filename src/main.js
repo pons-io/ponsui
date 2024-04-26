ff4205a2ee4a22dc8f492c91af57fd56505c2e58
dhershman1 <dustinh17@gmail.com>
2024-04-26 16:50:30 -0400

    Added: New skeleton loader

--- src/main.js
+++ src/main.js
@@ -3,12 +3,17 @@ import './assets/css/main.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import Skeleton from '@brayamvalero/vue3-skeleton'
+
 import App from './App.vue'
 import router from './router'
+
+import '@brayamvalero/vue3-skeleton/dist/style.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(Skeleton)
 
 app.mount('#app')
