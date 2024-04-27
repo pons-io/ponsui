356b020d7d91013698c3f958d79e121a3bb39eff
dhershman1 <dustinh17@gmail.com>
2024-04-27 18:24:02 -0400

    Added: Snackbar

--- src/main.js
+++ src/main.js
@@ -4,16 +4,19 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import Skeleton from '@brayamvalero/vue3-skeleton'
+import { SnackbarService } from 'vue3-snackbar'
 
 import App from './App.vue'
 import router from './router'
 
 import '@brayamvalero/vue3-skeleton/dist/style.css'
+import 'vue3-snackbar/styles'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(Skeleton)
+app.use(SnackbarService)
 
 app.mount('#app')
